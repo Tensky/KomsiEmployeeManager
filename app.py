25ad4ffd58d81b1450c0e044db883738353110b6
Tensky <serusial1122@gmail.com>
2019-12-13 20:49:11 +0700

    trylogin2

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def create_app():
 
     from main import main as main_blueprint
     app.register_blueprint(main_blueprint)
-    app.run(debug=True, port="8000")
+    app.run(port="8000")
     return app
 
 
